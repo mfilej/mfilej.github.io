19a240cff8d6f4f6d0b226d6d935ab8452c69fb3
Miha Filej <miha@filej.net>
2025-02-02 17:02:18 +0100

    Set up basic skeleton with Tailwind 4

--- _config.ts
+++ _config.ts
@@ -1,5 +1,9 @@
 import lume from "lume/mod.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
 
 const site = lume();
+
+site.use(tailwindcss());
+site.add("/style.css");
 
 export default site;
