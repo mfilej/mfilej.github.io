214968a7fccb77fe17b26c3cc680bd05e6933218
Miha Filej <miha@filej.net>
2025-02-12 19:27:16 +0100

    Move compiled twil pages as an archive

--- _config.ts
+++ _config.ts
@@ -4,17 +4,20 @@ import mdx from "lume/plugins/mdx.ts";
 import jsx from "lume/plugins/jsx.ts";
 import remark from "lume/plugins/remark.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
+import redirects from "lume/plugins/redirects.ts";
 
 const site = lume();
 
 site.add("/style.css");
 site.add("/rebox.js");
 site.add("/img");
+site.add("/twil");
 
 site.use(date());
 site.use(jsx());
 site.use(mdx());
 site.use(remark());
 site.use(tailwindcss());
+site.use(redirects());
 
 export default site;
